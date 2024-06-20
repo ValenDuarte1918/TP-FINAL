0cd1b99e335b9969cb6c09ac4196288742571f83
valenduarte1918 <valentinduarte1@hotmail.com>
2024-06-19 23:04:54 -0300

    Creacion de Modo Simulado, en donde se introduce de forma manual los dados

--- Funciones.h
+++ Funciones.h
@@ -6,8 +6,6 @@
 
 using namespace std;
 
-string nombre;
-
 void lanzarDados(int dados[7]) {
     // Inicializa el array a cero
     for (int i = 0; i < 7; ++i) {
@@ -19,7 +17,24 @@ void lanzarDados(int dados[7]) {
         dados[valor]++;
     }
 }
-
+void introducirDados(int dados[7]) {
+    // Inicializa el array a cero
+    for (int i = 0; i < 7; ++i) {
+        dados[i] = 0;
+    }
+    // Introduce los valores manualmente y cuenta los valores
+    cout << "Introduce los valores de los dados (6 valores): ";
+    for (int i = 0; i < 6; i++) {
+        int valor;
+        cin >> valor;
+        if (valor >= 1 && valor <= 6) {
+            dados[valor]++;
+        } else {
+            cout << "Valor invalido. Introduce un valor entre 1 y 6.\n";
+            --i;  // Permite reintroducir el valor
+        }
+    }
+}
 bool esEscalera(int dados[7]) {
     for (int i = 1; i <= 6; ++i) {
         if (dados[i] != 1) return false;
@@ -58,25 +73,28 @@ int calcularPuntaje(int dados[7]) {
     return suma;
 }
 
-void jugarRonda(int& puntajeTotal, int ronda, string nombre) {
+void jugarRonda(int& puntajeTotal, int ronda, string nombre, bool modoSimulado) {
     system("cls");
     int puntajeMaxRonda = 0;
     for (int i = 0; i < 3; ++i) {
         cout << "------------------------------------------------------------------\n";
-        cout << "TURNO DE:" << nombre <<  "| RONDA N " << ronda + 1 << " | PUNTAJE TOTAL: " << puntajeTotal << " PUNTOS\n";
+        cout << "TURNO DE: " << nombre <<  " | RONDA N " << ronda + 1 << " | PUNTAJE TOTAL: " << puntajeTotal << " PUNTOS\n";
         cout << "------------------------------------------------------------------\n";
         cout << "LANZAMIENTO N " << i + 1 << "\n";
         cout << "------------------------------------------------------------------\n";
 
-        cin.ignore();
-
         int dados[7] = {0};  // Array para contar la cantidad de cada valor de dado
-        lanzarDados(dados);
+        if (modoSimulado) {
+            introducirDados(dados);
+        } else {
+            lanzarDados(dados);
+        }
+
         cout << "MAXIMO PUNTAJE DE LA RONDA: " << puntajeMaxRonda << endl;
         cout << "TUS LANZAMIENTOS: ";
         for (int j = 1; j <= 6; ++j) {
             for (int k = 0; k < dados[j]; ++k) {
-                 cout << j << " ";
+                cout << j << " ";
             }
         }
         cout << endl;
@@ -93,9 +111,11 @@ void jugarRonda(int& puntajeTotal, int ronda, string nombre) {
         if (puntaje > puntajeMaxRonda) {
             puntajeMaxRonda = puntaje;
         }
-    // Pausa antes de continuar con el próximo lanzamiento
-    cout << "Presiona Enter para continuar...\n";
-    cin.ignore();
+
+        // Pausa antes de continuar con el próximo lanzamiento
+        cout << "Presiona Enter para continuar...\n";
+        cin.ignore();
+        cin.get();
     }
     puntajeTotal += puntajeMaxRonda;
     cout << "------------------------------------------------------------------\n";
@@ -103,13 +123,14 @@ void jugarRonda(int& puntajeTotal, int ronda, string nombre) {
     cout << "------------------------------------------------------------------\n";
     cout << "Presiona Enter para continuar a la siguiente ronda...\n";
     cin.ignore();
+    cin.get();
 }
 
-void jugarJuego(string nombre) {
+void jugarJuego(string nombre, bool modoSimulado) {
     int puntajeTotal = 0;
     int rondas = 0;
     while (puntajeTotal < 100) {
-        jugarRonda(puntajeTotal, rondas, nombre);
+        jugarRonda(puntajeTotal, rondas, nombre, modoSimulado);
         rondas++;
     }
     cout << "¡Ganaste la partida en " << rondas << " rondas!\n";
--- menu.h
+++ menu.h
@@ -3,11 +3,11 @@
 using namespace std;
 #ifndef MENU_H_INCLUDED
 #define MENU_H_INCLUDED
-void menu();
 
 void menu(){
     int opcion;
     string nombre;
+    bool modoSimulado = false;
     
     while(true){
         system("cls");
@@ -17,7 +17,7 @@ void menu(){
         
         cout << "1- Cargar Nombre" << endl;
         cout << "2- Jugar solo " << endl;
-        cout << "3- Jugar doble " << endl;
+        cout << "3- Modo Simulado " << endl;
         cout << "0- Salir" << endl;
         cout << "----------------" << endl;
         cout << "Opcion: ";
@@ -30,8 +30,12 @@ void menu(){
             getline(cin, nombre);
             break;
         case 2:
-            jugarJuego(nombre);
+            jugarJuego(nombre, modoSimulado);
             break;
+        case 3:
+                modoSimulado = true;
+                jugarJuego(nombre, modoSimulado);
+                break;
         case 0:
             return;
         }  
